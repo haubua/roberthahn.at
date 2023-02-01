95deb2788dfec9d9bbd8954e6e1c1eb121e134f4
Robert Hahn <robert.hahn91@gmx.at>
2023-02-01 21:10:26 +0100

    Started on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'roberthahn';
+export class AppComponent implements OnInit{
+images = ['compass-7592447_1920.jpg', 'golden-eagle-7555959_1920.jpg', 'ocean-7736669_1920.jpg'];
+currentImage = 0;
+showImage = true;
+
+  ngOnInit() {
+    this.slideshow();
+  }
+
+  slideshow() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true
+      }, 1)
+    } ,8000);
+  }
 }
